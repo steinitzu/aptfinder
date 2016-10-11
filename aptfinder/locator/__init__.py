54b57a49d181e69e3b857c0f56d1bddfd6a2a209
steinitzu <steini90@gmail.com>
2016-10-11 10:24:06 -0400

    omit null  price listings from search

--- aptfinder/locator/__init__.py
+++ aptfinder/locator/__init__.py
@@ -1,8 +1,6 @@
 from math import radians, sin, cos, asin, sqrt
 
 from .. import db
-from ..db.models import Apartment
-
 
 # Earth radius in km
 EARTH_RADIUS = 6371
@@ -18,38 +16,26 @@ def apartments_in_radius(center, radius_meters, bounds):
     circle with given center and radius.
     """
     rkm = radius_meters/1000
-    #s = db.Session()
 
-    #q = s.query(Apartment)
     bn, bs, be, bw = (bounds['north'], bounds['south'],
                       bounds['east'], bounds['west'])
 
     res = db.engine.execute(
         '''
         SELECT * FROM apartment WHERE
+        price is not %s AND
         latitude <= %s AND
         latitude >= %s AND
         longitude <= %s AND
         longitude >= %s;
         ''',
-        (bn, bs, be, bw))
+        (None, bn, bs, be, bw))
 
-    # Start by selecting all listings that fall within
-    # the circle's bounds (treat it like a rectangle)
-    # filterq = q.filter(Apartment.latitude != None,
-    #                    Apartment.latitude <= bn,
-    #                    Apartment.latitude >= bs,
-    #                    Apartment.longitude <= be,
-    #                    Apartment.longitude >= bw)
-
-    # Next, check haversine distance for found listings
-    # for a more precise result
     for apt in res:
         d = haversine((apt['latitude'], apt['longitude']), center)
         if d <= rkm:
             yield apt
     res.close()
-    #s.close()
 
 
 def haversine(pointa, pointb):
