e01d76c6baf909747e7cbf4478ab786aac878df4
steinitzu <steini90@gmail.com>
2016-10-14 13:24:37 -0400

    Local request test

--- testrequest.py
+++ testrequest.py
@@ -6,7 +6,7 @@ import pytest_benchmark
 def request_big_url():
     url = 'http://localhost:8000/get_apts?south=43.548689481127205&west=-79.78201725278637&north=43.84062756586321&east=-79.37824735846579&radius=16249.199467987859&center_lat=43.69465852349521&center_lng=-79.58013230562608&coordtype=degrees'
     r = requests.get(url)
-    return r.json()
+    return r
 
 
 def test_big_request(benchmark):
